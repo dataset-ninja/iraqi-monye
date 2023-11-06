d8134b69fbee5640733edf334d7c403fed54e100
erastov-alex <erastov.alex@gmail.com>
2023-11-06 12:02:44 +0300

    upd convert, settings, options, gitignore

--- src/convert.py
+++ src/convert.py
@@ -1,9 +1,11 @@
 import supervisely as sly
 import os
+import glob
 from dataset_tools.convert import unpack_if_archive
 import src.settings as s
 from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
+from supervisely.io.json import load_json_file
+from supervisely.io.fs import get_file_name, get_file_name_with_ext
 import shutil
 
 from tqdm import tqdm
@@ -69,17 +71,93 @@ def count_files(path, extension):
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
-    ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
-
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
-
-    # ... some code here ...
-
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
-
-    # return project
+    dataset_path = os.path.join("Iraqi Money","Project-1-JPG")
+    ann_path = os.path.join("Iraqi Money","Project-1-JPG","create_ml_labels.json")
+    batch_size = 30
+    ds_name = "ds"
 
 
+    def create_ann(image_path):
+        labels = []
+
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
+
+        image_name = get_file_name_with_ext(image_path)
+        ann_data = name_to_data[image_name]
+        for curr_ann_data in ann_data:
+            obj_class = meta.get_obj_class(curr_ann_data["label"])
+            coords = curr_ann_data["coordinates"]
+            left = int(coords["x"]) - int(coords["width"]) / 2
+            top = int(coords["y"]) - int(coords["height"]) / 2
+            right = left + int(coords["width"])
+            bottom = top + int(coords["height"])
+            rect = sly.Rectangle(left=left, top=top, right=right, bottom=bottom)
+            label = sly.Label(rect, obj_class)
+            labels.append(label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels)
+
+
+    ar250 = sly.ObjClass("250ar", sly.Rectangle)
+    en250 = sly.ObjClass("250en", sly.Rectangle)
+    ar500 = sly.ObjClass("500ar", sly.Rectangle)
+    en500 = sly.ObjClass("500en", sly.Rectangle)
+    ar1000 = sly.ObjClass("1000ar", sly.Rectangle)
+    en1000 = sly.ObjClass("1000en", sly.Rectangle)
+    ar5000 = sly.ObjClass("5000ar", sly.Rectangle)
+    en5000 = sly.ObjClass("5000en", sly.Rectangle)
+    ar10000 = sly.ObjClass("10000ar", sly.Rectangle)
+    en10000 = sly.ObjClass("10000en", sly.Rectangle)
+    ar25000 = sly.ObjClass("25000ar", sly.Rectangle)
+    en25000 = sly.ObjClass("25000en", sly.Rectangle)
+    ar50000 = sly.ObjClass("50000ar", sly.Rectangle)
+    en50000 = sly.ObjClass("50000en", sly.Rectangle)
+
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        obj_classes=[
+            ar250,
+            en250,
+            ar500,
+            en500,
+            ar1000,
+            en1000,
+            ar5000,
+            en5000,
+            ar10000,
+            en10000,
+            ar25000,
+            en25000,
+            ar50000,
+            en50000,
+        ]
+    )
+    api.project.update_meta(project.id, meta.to_json())
+
+    name_to_data = {}
+
+    ann = load_json_file(ann_path)
+    for curr_ann in ann:
+        name_to_data[curr_ann["imagename"].split("/")[-1]] = curr_ann["annotations"]
+
+    dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+    images_pathes = glob.glob(dataset_path + "/*/*.jpg")
+
+    progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
+    for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+        img_names_batch = [get_file_name_with_ext(im_path) for im_path in img_pathes_batch]
+
+        img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+        img_ids = [im_info.id for im_info in img_infos]
+
+        anns = [create_ann(image_path) for image_path in img_pathes_batch]
+        api.annotation.upload_anns(img_ids, anns)
+
+        progress.iters_done_report(len(img_names_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,37 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "Iraqi Money"
+PROJECT_NAME_FULL: str = "Iraqi Money"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Unknown()
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Domain.OCR()]
+CATEGORY: Category = Category.General()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.ObjectDetection()]
 
 RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
-    RELEASE_YEAR: int = None
+    RELEASE_YEAR: int = 2020
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = "https://www.kaggle.com/datasets/husamaamer/iraqi-currency-"
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 8060459
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/iraqi-monye"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = ["https://www.kaggle.com/datasets/husamaamer/iraqi-currency-"]
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -53,14 +53,14 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 # Use dict key to specify name for a button
 PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = ["Husam Aamer"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["https://www.kaggle.com/husamaamer"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = ["AppChief.net"]
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = ["https://appchief.net "]
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
 SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
